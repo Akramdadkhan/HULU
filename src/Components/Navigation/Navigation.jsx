32efc4322e6ad9f87ea6b75d81a7920ee364dbf8
AkramDadKhan1 <akram.khan@fyntune.com>
2022-09-28 12:53:39 +0530

    Done

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import request from "../Request/requests.js"
-import "./navigation.css"
+import React from "react";
+import request from "../Request/requests.js";
+import "./navigation.css";
+import * as mq from "../style/mediaQuries";
+import styled from "styled-components/macro";
+import useMediaQuery from "../style/useMediaQuery.jsx";
 
-function Navigation({setSelectedOption}) {
+function Navigation({ setSelectedOption }) {
+  const matches = useMediaQuery(`${mq.smForHook}`);
+
   return (
-    <div className='navigation'>
-        <h2 onClick={()=>setSelectedOption(request.fetchTrending)}>Trending</h2>
-        <h2 onClick={()=>setSelectedOption(request.fetchTopRated)}>Top Rated</h2>
-        <h2 onClick={()=>setSelectedOption(request.fetchActionMovies)}>Action</h2>
-        
-        <h2 onClick={()=>setSelectedOption(request.fetchHorrorMovies)}>Horror</h2>
-        <h2 onClick={()=>setSelectedOption(request.fetchRomanceMovies)}>Romance</h2>
-        
-        <h2 onClick={()=>setSelectedOption(request.fetchSciFi)}>Sci-fi</h2>
-        <h2 onClick={()=>setSelectedOption(request.fetchWestern)}>Westren</h2>
-        <h2 onClick={()=>setSelectedOption(request.fetchWestern)}>Animation</h2>
-        <h2 onClick={()=>setSelectedOption(request.fetchTV)}>TV Movie</h2>
-      
+    <div
+      css={`
+        ${mq.sm} {
+          // display: flex;
+          // flex-direction: column;
+          // gap: 1rem;
+          // align-item: center;
+        }
+      `}
+    >
+      {matches ? (
+        <div>Hello</div>
+      ) : (
+        <div className="navigation">
+          <h2 onClick={() => setSelectedOption(request.fetchTrending)}>
+            Trending
+          </h2>
+          <h2 onClick={() => setSelectedOption(request.fetchTopRated)}>
+            Top Rated
+          </h2>
+          <h2 onClick={() => setSelectedOption(request.fetchActionMovies)}>
+            Action
+          </h2>
+
+          <h2 onClick={() => setSelectedOption(request.fetchHorrorMovies)}>
+            Horror
+          </h2>
+          <h2 onClick={() => setSelectedOption(request.fetchRomanceMovies)}>
+            Romance
+          </h2>
+
+          <h2 onClick={() => setSelectedOption(request.fetchSciFi)}>Sci-fi</h2>
+          <h2 onClick={() => setSelectedOption(request.fetchWestern)}>
+            Westren
+          </h2>
+          <h2 onClick={() => setSelectedOption(request.fetchWestern)}>
+            Animation
+          </h2>
+          <h2 onClick={() => setSelectedOption(request.fetchTV)}>TV Movie</h2>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
